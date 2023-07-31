e49b0cd8488a283ab38d093fc9a3f2e3dfceed76
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 11:27:03 +0500

    Client Section

---
+++ layouts/Clients/Clients.tsx
@@ -0,0 +1,40 @@
+const Clients = () => {
+  return (
+    <>
+      <div id="partners" className="pt-150"></div>
+      <section className="brand-area pt-130 pb-120">
+        <div className="container">
+          <div className="row brand-active">
+            <div className="col-xl-2">
+              <div className="slide_img">
+                <a href="#"><img src="./assets/img/brand/divan.svg" alt=""/></a>
+              </div>
+            </div>
+            <div className="col-xl-2">
+              <div className="slide_img">
+                <a href="#"><img src="./assets/img/brand/yelken.svg" alt=""/></a>
+              </div>
+            </div>
+            <div className="col-xl-2">
+              <div className="slide_img">
+                <a href="#"><img src="./assets/img/brand/03.png" alt=""/></a>
+              </div>
+            </div>
+            <div className="col-xl-2">
+              <div className="slide_img">
+                <a href="#"><img src="./assets/img/brand/04.png" alt=""/></a>
+              </div>
+            </div>
+            <div className="col-xl-2">
+              <div className="slide_img">
+                <a href="#"><img src="./assets/img/brand/05.png" alt=""/></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+};
+
+export default Clients;
