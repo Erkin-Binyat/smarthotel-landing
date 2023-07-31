a7b162312f619cd42fcf6160dbcb31f8c1134ebb
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 11:28:39 +0500

    Layout exports

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,5 +1,12 @@
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
 import Hero from "./Hero/Hero";
+import Features from "./Features/Features";
+import Download from "./Download/Download";
+import Pricing from "./Pricing/Pricing";
+import Clients from "./Clients/Clients";
+import Services from "./Services/Services";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
 
-export { Header, Sidebar, Hero };
+export { Header, Sidebar, Hero, Features, Download, Pricing, Clients, Services, Contact, Footer };
