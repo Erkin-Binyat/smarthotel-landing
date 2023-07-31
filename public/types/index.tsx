cf35f8f24a2ebb9573d7836eceaf4e47337a33d0
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 11:31:27 +0500

    Prop Types

---
+++ public/types/index.tsx
@@ -0,0 +1,11 @@
+export type CoreFeatureProps = {
+  activeTab: boolean,
+  service: {
+    content: {
+      title: string,
+      heading: string,
+      desc: string,
+      img: string
+    }
+  }
+};
