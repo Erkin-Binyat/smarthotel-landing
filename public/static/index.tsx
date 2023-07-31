ee60b9b090c3e6ed28be3e791685e284dbae35d7
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 11:31:08 +0500

    Static Data

---
+++ public/static/index.tsx
@@ -0,0 +1,125 @@
+export const features = [
+  {
+    img: "./assets/img/icon/reception.svg",
+    title: "frontdesk",
+    content: [
+      {
+        name: "reservation",
+      },
+      {
+        name: "multipleUser",
+      },
+      {
+        name: "room",
+      },
+      {
+        name: "modify",
+      },
+      {
+        name: "assignTasks",
+      },
+      {
+        name: "inventory",
+      }
+    ]
+  },
+  {
+    img: "./assets/img/icon/housekeeping.svg",
+    title: "housekeeping",
+    content: [
+      {
+        name: "controlStatus"
+      },
+      {
+        name: "trackTasks"
+      },
+      {
+        name: "staffManagement"
+      },
+      {
+        name: "controlManage"
+      }
+    ]
+  },
+  {
+    img: "./assets/img/icon/reporting.svg",
+    title: "reporting",
+    content: [
+      {
+        name: "overallPerform"
+      },
+      {
+        name: "accountingReports"
+      },
+      {
+        name: "financialSit"
+      },
+      {
+        name: "evaluatePerform"
+      },
+      {
+        name: "historicalData"
+      },
+      {
+        name: "integrationExcel"
+      },
+      {
+        name: "revenueProject"
+      }
+    ]
+  },
+  {
+    img: "./assets/img/icon/users.svg",
+    title: "usage",
+    content: [
+      {
+        name: "keepOperations"
+      },
+      {
+        name: "dataSecurity"
+      },
+      {
+        name: "distanceTraining"
+      }
+    ]
+  }
+];
+
+export const coreFeatures = [
+  {
+    title: "Detailed reports",
+    content: {
+      title: "Reporting & Analytics",
+      heading: "Detailed reports",
+      desc: "Make sure your strategy adjustments are based on data, not on your gut instincts. Browse through all reports in the SHMS and really grasp how your business is going.",
+      img: "../assets/img/feature/report.jpg"
+    }
+  },
+  {
+    title: "Well-organized lists",
+    content: {
+      title: "",
+      heading: "Well-organized lists",
+      desc: "Consult convenient housekeeping and guest lists. Immediately add extra comments to keep your team informed.",
+      img: "../assets/img/feature/HOUSE.png"
+    }
+  },
+  {
+    title: "User Experience",
+    content: {
+      title: "Progress & Customizations",
+      heading: "User Experience",
+      desc: "Get painless implementation, easy training, and an intuitive interface without sacrificing transparency.",
+      img: "../assets/img/feature/user.png"
+    }
+  },
+  {
+    title: "Check-in & Check-out",
+    content: {
+      title: "Progress & Customizations",
+      heading: "Speed up your check-in & check-out",
+      desc: "Get a clear overview of all arriving guests and check-in guests in no-time, even group reservations. Quickly check who has already paid or not and speed up the process.",
+      img: "../assets/img/feature/reservation.png"
+    }
+  }
+];
