873571b5f5153c2ca5bf6faacf9fd6a108d2a6e8
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 11:30:03 +0500

    Component exports

---
+++ components/index.tsx
@@ -0,0 +1,5 @@
+import FeatureCard from "./FeatureCard/FeatureCard";
+import LanguageSwitcher from "./LanguageSwitcher/LanguageSwitcher";
+import CoreFeature from "./CoreFeature/CoreFeature";
+
+export { FeatureCard, LanguageSwitcher, CoreFeature };
