0d90e7c405a639f9968a869d520f6acda5a74d90
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 11:28:59 +0500

    Footer section

---
+++ layouts/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="footer-area fot-area-03 foot-area-06 white-bg pos-rel pt-80">
+		<div className="container">
+			<div className="copy-right-area copy-right-03 pt-35">
+				<div className="row">
+					<div className="col-12">
+						<div className="copyright mb-35 d-flex justify-content-center">
+							<p>SmartHotel © 2023 Copyright, All Rights Reserved.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+  )
+};
+
+export default Footer;
