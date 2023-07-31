66c3464ddceec9c200f1b2f2539010449596d9c1
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 15:53:04 +0500

    Fixed jquery error

--- layouts/Clients/Clients.tsx
+++ layouts/Clients/Clients.tsx
@@ -1,9 +1,4 @@
 import React from "react";
-var $ = require("jquery");
-if (typeof window !== "undefined") {
-  // Client-side-only code
-  window.$ = window.jQuery = require("jquery");
-}
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import dynamic from "next/dynamic";
