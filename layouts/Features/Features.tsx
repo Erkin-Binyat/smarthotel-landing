70de44f662fbda54edeb9997a99a3d08516b02ed
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 11:44:12 +0500

    Added new text

--- layouts/Features/Features.tsx
+++ layouts/Features/Features.tsx
@@ -14,10 +14,20 @@ const Features = () => {
 		<div className="container">
 			<div className="row justify-content-center">
 				<div className="col-xxl-6 col-xl-6 wow fadeInUp2 animated" data-wow-delay="0.1s">
-					<div className="section-title blue-title text-center pr-50 pl-50 mb-80">
+					<div className="section-title blue-title text-center pr-50 pl-50 mb-40">
 						<h2>Complete solution for
 							hotel management</h2>
 					</div>
+				</div>
+			</div>
+			<div className="row justify-content-start mb-20">
+				<div className="col-xxl-6 col-xl-6 ">
+					<p className="text-left">SmartHotel is a hotel management system developed by local software specialists. Managing hotel operations is simplified with SmartHotel management technology, which controls processes for improved efficiency. SmartHotel offers an intuitive user interface, comprehensive functionality for all areas of hotel management, secure data storage and meets the needs of all types of hotels. </p>
+				</div>
+			</div>
+			<div className="row justify-content-end mb-40">
+				<div className="col-xxl-6 col-xl-6 ">
+					<p className="text-center">The software provides key features as; front desk & back office operations (with integrated accounting), reservation management, housekeeping, assigning rooms, managing room inventory, and performing accounting and billing functions. </p>
 				</div>
 			</div>
 			<div className="row">{renderFeatures}</div>
