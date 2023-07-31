b5176b08c5bfc67c803d072583b4e9c974c468be
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-31 11:29:38 +0500

    Feature Component

--- components/FeatureCard/FeatureCard.tsx
+++ components/FeatureCard/FeatureCard.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from "next-i18next";
+
 type FeatureProps = {
   title: string,
   img: string,
@@ -6,6 +8,8 @@ type FeatureProps = {
 }
 
 const FeatureCard = ({title, img, content, index}: FeatureProps) => {
+  const { t } = useTranslation();
+
   return (
     <div className="col-xxl-3 col-xl-3 col-lg-6 col-md-6">
     <div className="do_box do_box_02 grey-bg text-center mb-30">
@@ -13,11 +17,11 @@ const FeatureCard = ({title, img, content, index}: FeatureProps) => {
         <img alt={title} src={img}
           style={{height: "40px", marginBottom: "20px"}} />
       </div>
-      <h4>{title}</h4>
+      <h4>{t(`features.${title}`)}</h4>
       <ul className="pr_list text-xxl-start">
         {content.map((data: any, index) => {
           const { name } = data;
-          return <li key={index}><i className="far fa-check"></i> {name}</li>
+          return <li key={index}><i className="far fa-check"></i> {t(`features.${name}`)}</li>
         })}
       </ul>
     </div>
