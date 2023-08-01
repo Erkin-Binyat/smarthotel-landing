11351537896b12a95fb04c09e9e33f6dbed05d5b
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-08-01 14:01:17 +0500

    Hero section

--- layouts/Hero/Hero.tsx
+++ layouts/Hero/Hero.tsx
@@ -6,18 +6,18 @@ const Hero = () => {
   return (
     <>
       <div className="hero-area hero-area-02 hero-area-06 pos-rel">
-        <div className="hero_right_img d-none d-lg-block animation" data-aos={"zoom-in"}>
-          <img className="img-fluid" src="./assets/img/slider/slider6.png" alt="" />
+        <div className="hero_right_img d-none d-lg-block">
+          <img className="img-fluid" src="./assets/img/slider/slider6.png" alt="" data-aos={"zoom-in"} />
         </div>
         <img className="shape-one" src="./assets/img/shape/16.png" alt="" />
         <div className="container">
           <div className="row align-items-center">
             <div className="col-xxl-5 col-xl-5 col-lg-6">
               <div className="hero__content hero__content__02 hero__content__06 text-left">
-                <h2 id="header-intro" className="main-title mb-20 animation" data-aos={"fade-up"}>{t("hero.title")}
+                <h2 id="header-intro" className="main-title mb-20" data-aos={"fade-up"}>{t("hero.title")}
                 </h2>
-                <p id="headerAbout" className="animation" data-aos={"fade-up"}>{t("hero.description")}</p>
-                <ul className="btn-list mt-35 animation" data-aos={"fade-up"}>
+                <p id="headerAbout" data-aos={"fade-up"}>{t("hero.description")}</p>
+                <ul className="btn-list mt-35" data-aos={"fade-up"}>
                   <li><a className="theme_btn theme_btn_bg" href="#features">discover more
                     <i className="fas fa-chevron-right"></i></a>
                   </li>
