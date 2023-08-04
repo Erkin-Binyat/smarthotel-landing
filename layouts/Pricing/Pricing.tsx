2bd8b039da0668b30a31a794e5f86c6f59fcf610
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-08-04 14:43:37 +0500

    Pricing section fixed

--- layouts/Pricing/Pricing.tsx
+++ layouts/Pricing/Pricing.tsx
@@ -1,33 +1,58 @@
 import { useTranslation } from "next-i18next";
+import { FaCheck } from "react-icons/fa";
 
 const Pricing = () => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <>
-      <div id="pricing" className="pt-150"></div>
-      <section className="container-fluid plan-area pt-10" style={{ backgroundColor: "#266de8" }}>
-        <div className="container">
-          <div className="row">
-            <div className="plan pos-rel active text-start mb-20 m-60">
-              <div className="pr_head">
-                <span>{t("pricing.title")}</span>
-                <h3 className="pt-20">{t("pricing.heading")}</h3>
-              </div>
-              <div className="pr_body mb-30">
-                <ul className="pr_list">
-                  <li><i className="far fa-check"></i> {t("pricing.description")}</li>
-                </ul>
-              </div>
-              <div className="pr_footer">
-                <a href="#contactUs" className="theme_btn pr_btn">{t("pricing.request")} <i className="fas fa-chevron-right"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  )
+    return (
+        <>
+            <section className="plan-area pt-125" id="pricing">
+                <div className="container">
+                    <div className="plan-wrapper">
+                        <div className="row">
+                            <div className="col-xxl-6 col-xl-6">
+                                <div className="section-title pt-80 mb-30">
+                                    <h5>{t("pricing.title")}</h5>
+                                    <h2>{t("pricing.heading")}</h2>
+                                    <p className="pt-30">{t("pricing.description")}</p>
+                                </div>
+                            </div>
+                            <div className="col-xxl-5 offset-xxl-1 col-xl-5 offset-xl-1">
+                                <div className="dnxte-content-toggle">
+                                    <div className="dnxte-content-toggle-body">
+                                        <div className="dnxte-content-toggle-front">
+                                            <div className="row pb-100">
+                                                <div className="col-xxl-12 col-xl-8 col-lg-6 col-md-6">
+                                                    <div className="plan pos-rel active text-center mb-30">
+                                                        <div className="pr_head"></div>
+                                                        <div className="pr_body mb-20">
+                                                            <ul className="pr_list">
+                                                                <li><FaCheck size={12} />  {t("pricing.dashboard")}</li>
+                                                                <li><FaCheck size={12} />  {t("pricing.reception")}</li>
+                                                                <li><span><FaCheck size={12} />  {t("pricing.multipleUser")}</span></li>
+                                                                <li><FaCheck size={12} />  {t("pricing.housekeeping")}</li>
+                                                                <li><FaCheck size={12} />  {t("pricing.support")}</li>
+                                                            </ul>
+                                                        </div>
+                                                        <div className="pr_footer">
+                                                            <a href="#contactUs" className="theme_btn pr_btn">{t("pricing.request")} <i className="fas fa-chevron-right"></i></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="has-plan-border">
+                        <div className="border-bottom"></div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 };
 
 export default Pricing;
