b94e87284e7ffd177bd6a08b547d651777941396
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-08-04 21:06:31 +0500

    Component exports

--- components/index.tsx
+++ components/index.tsx
@@ -2,5 +2,6 @@ import FeatureCard from "./FeatureCard/FeatureCard";
 import LanguageSwitcher from "./LanguageSwitcher/LanguageSwitcher";
 import CoreFeature from "./CoreFeature/CoreFeature";
 import Preloader from "./Preloader/Preloader";
+import Accordion from "./Accordion/Accordion";
 
-export { FeatureCard, LanguageSwitcher, CoreFeature, Preloader };
+export { FeatureCard, LanguageSwitcher, CoreFeature, Preloader, Accordion };
