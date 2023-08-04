5c052edad70e6790fe801a107905a0cd8a027578
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-08-04 15:11:53 +0500

    Features section

--- layouts/Features/Features.tsx
+++ layouts/Features/Features.tsx
@@ -6,39 +6,41 @@ import { useTranslation } from "next-i18next";
 
 const Features = () => {
 	const { t } = useTranslation();
-  const renderFeatures = useMemo(() => {
-    return features ? features.map((feature, index) => {
-      return <FeatureCard key={index} index={index} {...feature} /> }) : null
-  }, [features]);
-  
-  return (
-    <section className="what-do-area-02 what-do-area-06 pt-140 pb-100" id="about">
-		<div className="container">
-			<div className="row justify-content-center">
-				<div className="col-xxl-6 col-xl-6 aos-init aos-animate" data-aos="fade-up">
-					<div className="section-title blue-title text-center pr-50 pl-50 mb-40">
-						<h2>Complete solution for
-							hotel management</h2>
+	const renderFeatures = useMemo(() => {
+		return features ? features.map((feature, index) => {
+			return <FeatureCard key={index} index={index} {...feature} />
+		}) : null
+	}, [features]);
+
+	return (
+		<>
+			<section className="what-do-area-02 what-do-area-06 pt-140 pb-100" id="aboutUs">
+				<div className="container">
+					<div className="row justify-content-center">
+						<div className="col-xxl-6 col-xl-6 aos-init aos-animate" data-aos="fade-up">
+							<div className="section-title blue-title text-center pr-50 pl-50 mb-40">
+								<h2>Complete solution for
+									hotel management</h2>
+							</div>
+						</div>
 					</div>
+					<div className="row justify-content-start mb-20">
+						<div className="col-xxl-8 col-xl-8 aos-init aos-animate" data-aos="fade-up">
+							<p className="text-left" style={{ fontSize: "18px", color: "rgba(0, 0, 0, 0.7" }}>{t("about.desc1")}</p>
+						</div>
+					</div>
+					<div className="row justify-content-end mb-20 animation" data-aos="fade-up">
+						<div className="col-xxl-8 col-xl-8 ">
+							<p style={{ fontSize: "18px", color: "rgba(0, 0, 0, 0.7" }}>{t("about.desc2")}</p>
+							<p className="pt-20" style={{ fontSize: "18px", color: "rgba(0, 0, 0, 0.7" }}  id="features">{t("about.desc3")}</p>
+						</div>
+					</div>
+					<div className="row pt-20">{renderFeatures}</div>
+					<div style={{ paddingBottom: "20px" }} id="management"></div>
 				</div>
-			</div>
-			<div className="row justify-content-start mb-50">
-				<div className="col-xxl-8 col-xl-8 aos-init aos-animate" data-aos="fade-up">
-					<p className="text-left" style={{fontSize: "18px", color: "rgba(0, 0, 0, 0.7"}}>{t("about.desc1")}</p>
-				</div>
-			</div>
-			<div className="row justify-content-end mb-40 animation" data-aos="fade-up">
-				<div className="col-xxl-8 col-xl-8 ">
-					<p style={{fontSize: "18px", color: "rgba(0, 0, 0, 0.7"}}>{t("about.desc2")}</p>
-					<p className="pt-50" style={{fontSize: "18px", color: "rgba(0, 0, 0, 0.7"}}>{t("about.desc3")}</p>
-				</div>
-			</div>
-			<div id="features" className="pt-100"></div>
-			<div className="row">{renderFeatures}</div>
-			<div style={{paddingBottom: "20px"}} id="management"></div>
-		</div>
-	</section>
-  )
+			</section>
+		</>
+	)
 };
 
 export default Features;
