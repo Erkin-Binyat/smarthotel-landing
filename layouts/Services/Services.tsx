ab2861dc84ea210c0487dbac73f575abbd77f3a4
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-08-04 21:07:24 +0500

    Fixed mobile & desktop in services

--- layouts/Services/Services.tsx
+++ layouts/Services/Services.tsx
@@ -1,12 +1,23 @@
 "use client"
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { coreFeatures } from "@/public/static";
-import { CoreFeature } from "@/components";
+import { CoreFeature, Accordion } from "@/components";
 
 const Services = () => {
-	const [ services, setServices ] = useState(coreFeatures);
-	const [ index, setIndex ] = useState(1);
+	const [services, setServices] = useState(coreFeatures);
+	const [index, setIndex] = useState(1);
+	const [isMobile, setIsMobile] = useState(false);
+	const width = window.innerWidth;
 
+	useEffect(() => {
+		if (width < 560) {
+			setIsMobile(true)
+		} else {
+			setIsMobile(false);
+		};
+	}, [width]);
+
+	// Rendering service header
 	const serviceHeading = useMemo(() => {
 		return services ? services.map((service, i) => {
 			const { title } = service;
@@ -15,7 +26,7 @@ const Services = () => {
 
 			return (
 				<li className="nav-item" role="presentation" key={i}>
-					<a className={activeTab ? "nav-link active" : "nav-link"} style={{cursor: "pointer"}} id="home-tab" onClick={() => setIndex(active)} role="tab" aria-controls="home" aria-selected="true">
+					<a className={activeTab ? "nav-link active" : "nav-link"} style={{ cursor: "pointer" }} id="home-tab" onClick={() => setIndex(active)} role="tab" aria-controls="home" aria-selected="true">
 						<i className="flaticon-product"></i> {title}
 					</a>
 				</li>
@@ -23,36 +34,48 @@ const Services = () => {
 		}) : null;
 	}, [services, index]);
 
+	// Rendering service content | Desktop
 	const serviceContent = useMemo(() => {
 		return services ? services.map((service, i) => {
 			const active = i + 1;
 			const activeTab = active === index;
 
-			return <CoreFeature service={service} key={i} activeTab={activeTab}/> 
+			return <CoreFeature service={service} key={i} activeTab={activeTab} />
 		}) : null
 	}, [services, index]);
 
-  return (
-    <section className="services-area services-area-pb grey-bg pt-125 pb-100">
-		<div className="container">
-			<div className="row">
-				<div className="col-xxl-6 offset-xxl-3 col-xl-8 offset-xl-2">
-					<div className="section-title blue-title text-center mb-30">
-						<h5>Core Features</h5>
-						<h2>We create software for your
-							business solutions</h2>
+	const serviceAccordion = useMemo(() => {
+		return services ? services.map((service, index) => {
+			return <Accordion service={service} index={index} key={index} />
+		}) : null;
+	}, [services]);
+
+	return (
+		<section className="services-area services-area-pb grey-bg pt-125 pb-100">
+			<div className="container">
+				<div className="row">
+					<div className="col-xxl-6 offset-xxl-3 col-xl-8 offset-xl-2">
+						<div className="section-title blue-title text-center mb-30">
+							<h5>Core Features</h5>
+							<h2>We create software for your
+								business solutions</h2>
+						</div>
 					</div>
 				</div>
+				{isMobile ? <div className="accordion">{serviceAccordion}</div>
+					:
+					<>
+						<ul className="nav nav-tabs" id="myTab" role="tablist">
+							{serviceHeading}
+						</ul>
+						<div className="tab-content" id="myTabContent">
+							{serviceContent}
+						</div>
+					</>
+				}
 			</div>
-			<ul className="nav nav-tabs" id="myTab" role="tablist">
-				{serviceHeading}
-			</ul>
-			<div className="tab-content" id="myTabContent">
-				{serviceContent}
-			</div>
-		</div>
-	</section>
-  )
+		</section>
+	)
 };
 
 export default Services;
