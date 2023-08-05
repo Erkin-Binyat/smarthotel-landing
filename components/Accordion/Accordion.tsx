8e7117e41b6cecc7fe54ecab0d8cc84cfd8e2eb6
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-08-05 16:25:19 +0500

    Added dictionary to Accordion

--- components/Accordion/Accordion.tsx
+++ components/Accordion/Accordion.tsx
@@ -1,11 +1,13 @@
 import useToggle from "@/hooks/useToggle";
 import { CoreFeatureProps } from "@/public/types";
 import { FaChevronDown } from "react-icons/fa";
+import { useTranslation } from "next-i18next";
 
-const Accordion = ({ service, index }: CoreFeatureProps) => {
+const Accordion = ({ service }: CoreFeatureProps) => {
   const { title, content: { desc, img } } = service;
   const { open, toggle } = useToggle();
-  
+  const { t } = useTranslation();
+
   // Button style
   const btnStyle = {
     width: "100%",
@@ -22,14 +24,14 @@ const Accordion = ({ service, index }: CoreFeatureProps) => {
     <div className="accordion-item">
       <h2 className="accordion-header" id="headingOne">
         <button onClick={() => toggle()} style={btnStyle} aria-expanded="true" aria-controls="collapse">
-          {title} <span style={{transform: open ? "rotate(-180deg)" : "rotate(0)", transition: "all .3s ease"}}><FaChevronDown/></span>
+          {t(`coreFeatures.${title}`)} <span style={{transform: open ? "rotate(-180deg)" : "rotate(0)", transition: "all .3s ease"}}><FaChevronDown/></span>
         </button>
       </h2>
       <div className={open ? "accordion-collapse collapse show" : "accordion-collapse collapse"} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
         <div className="accordion-body">
           <div className="feature_wrapper_02 mb-30">
             <div className="section-title text-left mb-35">
-              <p className="pt-30">{desc}</p>
+              <p className="pt-30">{t(`coreFeatures.${desc}`)}</p>
             </div>
             <img className="img-fluid" style={{ borderRadius: "12px" }} src={img} alt="" />
           </div>
